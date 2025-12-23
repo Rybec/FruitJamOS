00a2ee71c17791f8f4a57f3a60ff508b145d493b
Rybec Arethdar <rybecarethdar@gmail.com>
2025-12-23 00:11:34 +0000

    SGL header file

---
+++ sgl.h
@@ -0,0 +1,33 @@
+#include "spi1.h"
+#include "st7789.h"
+
+enum SGL_driver {
+	ST7789
+};
+
+
+// For multiscreen support, this will need
+// a special data field for tracking which
+// interface the screen is on (spi1, hstx...)
+// as well as things like the CS pin or I2C
+// address. 
+
+typedef struct {
+	uint16_t width;
+	uint16_t height;
+	enum SGL_driver driver;
+	uint16_t *buffer;
+} SGL_display;
+
+
+
+
+
+
+void SGL_init();
+
+SGL_display *SGL_create_display(enum SGL_driver driver);
+
+void SGL_destroy_display(SGL_display *display);
+
+
