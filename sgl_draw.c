c219c3610f939a862943585c758b4b8ae86c2897
Rybec Arethdar <rybecarethdar@gmail.com>
2025-12-22 22:14:52 -0900

    Corrected data types

--- sgl_draw.c
+++ sgl_draw.c
@@ -14,10 +14,10 @@ typedef struct {
 */
 
 void SGL_DRAW_hline(SGL_display *display,
-                    uint8_t x0, uint8_t x1,
-                    uint8_t y, uint16_t color) {
-	uint8_t _x0;
-	uint8_t _x1;
+                    uint16_t x0, uint16_t x1,
+                    uint16_t y, uint16_t color) {
+	uint16_t _x0;
+	uint16_t _x1;
 
 	if (x0 <= x1) {
 		_x0 = x0;
@@ -26,8 +26,8 @@ void SGL_DRAW_hline(SGL_display *display,
 		_x0 = x1;
 		_x1 = x0;
 	}
-	
-	for (uint8_t x = _x0; x < x1; x++)
+
+	for (uint16_t x = _x0; x < _x1; x++)
 		display->buffer[y * 320 + x] = color;
 }
 
--- sgl_draw.h
+++ sgl_draw.h
@@ -1,4 +1,4 @@
 
 void SGL_DRAW_hline(SGL_display *display,
-                    uint8_t x0, uint8_t x1,
-                    uint8_t y, uint16_t color);
+                    uint16_t x0, uint16_t x1,
+                    uint16_t y, uint16_t color);
