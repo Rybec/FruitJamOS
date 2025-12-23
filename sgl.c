d8d7751d6629ad2dc85a00cf9c2c5bab14a30b0f
Rybec Arethdar <rybecarethdar@gmail.com>
2025-12-23 00:10:34 +0000

    Added display object creation and free functions to SGL

--- sgl.c
+++ sgl.c
@@ -2,9 +2,59 @@
 
 /* Designed for real-time rendering
 on microcontrollers.  Uses patterns
-from SDL, adapted to low resource
-platforms. */
+from SDL, adapted to low resource,
+baremetal platforms. */
 
+#include <stdlib.h>
+
+#include "pico/stdlib.h"
+#include "pico/malloc.h"
+
+#include "spi1.h"
+#include "st7789.h"
+#include "sgl.h"
+
+
+void SGL_init() {
+	SPI1_init();
+}
+
+/*
+typedef struct {
+	uint16_t width;
+	uint16_t height;
+	enum SGL_Driver driver;
+	uint16_t *buffer;
+} SGL_display;
+*/
+
+SGL_display *SGL_create_display(enum SGL_driver driver) {
+	switch (driver) {
+		case ST7789:
+			uint16_t *buffer = calloc(320 * 240, 2);
+
+			ST7789_init(ROT90, ST7789_DC, ST7789_RST, ST7789_CS);
+			ST7789_set_framebuffer(buffer, 320, 240);
+
+			SGL_display *display = malloc(sizeof(SGL_display));
+
+			display->width = 320;
+			display->height = 240;
+			display->driver = driver;
+			display->buffer = buffer;
+
+			ST7789_blit();
+
+			return display;
+		default:
+			return NULL;
+	}
+}
+
+
+void SGL_destroy_display(SGL_display *display) {
+	free(display->buffer);
+}
 
 
 /*
